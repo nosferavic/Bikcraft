138272c88bfdbe42cb68f31febf24002fbf1d4a8
Victor Viana <victor.deviana@outlook.com>
2024-04-10 20:52:53 -0300

    question-expanded-with-js

--- js/script.js
+++ js/script.js
@@ -10,13 +10,28 @@ function ativarLink(link) {
 links.forEach(ativarLink);
 // ativar itens do orçamento
 const parametros = new URLSearchParams(location.search);
-
-function ativarProduto(parametro){
-  const elemento = document.getElementById(parametro)
-  if(elemento){
-    elemento.checked = true
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
   }
 }
+parametros.forEach(ativarProduto);
+// perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
 
-parametros.forEach(ativarProduto)
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
 
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
