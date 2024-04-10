30702ce9d1ce4fe64f2387f80698532135b13e51
Victor Viana <victor.deviana@outlook.com>
2024-04-10 20:18:55 -0300

    links-with-js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
+//ativar links no menu
 const links = document.querySelectorAll(".header-menu a");
-
 function ativarLink(link) {
   const url = window.location.href;
   const href = link.href;
-
   if (url.includes(href)) {
     link.classList.add("ativo");
   }
 }
-
 links.forEach(ativarLink);
+// ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro)
+  if(elemento){
+    elemento.checked = true
+  }
+}
+
+parametros.forEach(ativarProduto)
+
