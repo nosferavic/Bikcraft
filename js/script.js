2f4dcca6ac8fe44c9119d18cdb239bb0924a84b5
Victor Viana <victor.deviana@outlook.com>
2024-04-10 21:30:09 -0300

    plugin-animate

--- js/script.js
+++ js/script.js
@@ -37,11 +37,16 @@ const galeriaContainer = document.querySelector(".bicicleta-imagens");
 function trocarImagem(event) {
   const img = event.currentTarget;
   const media = matchMedia("(min-width:1000px)").matches;
-if(media){
-  galeriaContainer.prepend(img)
-}
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
 }
 function eventosGaleria(img) {
   img.addEventListener("click", trocarImagem);
 }
 galeria.forEach(eventosGaleria);
+
+//animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
